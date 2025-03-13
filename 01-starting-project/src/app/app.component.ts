5087d0e9a5f82623dc008368f8dcc75540309d5d
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-13 11:22:08 -0500

    removed signals and defined component inputs instead.

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users'; // we export users = DUMMY_USERS below; this is how we can expose this imported data in the template of this component.
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,6 @@ import { UserComponent } from "./user/user.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+}
--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length) // this should give you a random number between zero and the highest available index in the array.
-
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,16 +7,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length) // this shoul
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]) // draws the random user in the index.
+  @Input() avatar!: string; // With @Input added, we can then go to the place where we use the component as an element (app.component.html), and set the avatar property.
+  @Input() name!: string;
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar) // this is how to compute value when using SIGNALS. Now why would you do this? When you use a computed function, Angular automatically analyzes whether you are reading a signal value inside the function you passed, and Angular sets up a subscription to that signal thats being used (selectedUser signal). When that signal recieves a value, Angular will RECOMPUTE the imagePath.
-
-  // get imagePath() { // getter; method (a function inside a class). thats usable like a property. It doesn't need to be called and executed EXPLICITLY and meant to be produced and to return a value.
-  //   return 'assets/users/' + this.selectedUser.avatar // we use the 'this.' keyword to access this class.
-  // }
-
-  onSelectUser() { // regular method that can added to any class.
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length) // added this to generate a new, locally scoped randomIndex thats only available in this.selectedUser. We use this to extract and store a new user from DUMMY_USERS. * Lines 13 through 23 is how we update STATE.
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUser() {}
 }
