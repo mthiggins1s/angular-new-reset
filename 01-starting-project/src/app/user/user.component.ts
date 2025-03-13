52133902485da70eb7baf138a6c75cf899ad4eff
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-13 13:15:32 -0500

    added output improt

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
